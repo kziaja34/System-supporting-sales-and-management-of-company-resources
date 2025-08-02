c4507184e0a8303918a26cba2b06bd6e4c2da97f
Karol Ziaja <kziaja34@gmail.com>
2025-08-03 01:03:55 +0200

    generic crud service created

---
+++ SSSMCR/SSSMCR.ApiService/Services/GenericService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSSMCR.ApiService.Database;
+using SSSMCR.ApiService.Services.Interfaces;
+
+namespace SSSMCR.ApiService.Services;
+
+public class GenericService<T> : IGenericService<T> where T : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+    
+    public GenericService(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<T> CreateAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity == null) return;
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync() =>
+        await _dbSet.ToListAsync();
+
+    public async Task<T> GetByIdAsync(int id) =>
+        await _dbSet.FindAsync(id);
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ SSSMCR/SSSMCR.ApiService/Services/Interfaces/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace SSSMCR.ApiService.Services.Interfaces;
+using System.Threading.Tasks;
+
+public interface IGenericService<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+}
