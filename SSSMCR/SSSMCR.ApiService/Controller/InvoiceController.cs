c588a4b8993a67118dd5045ce2ddbbdcd7916ada
Karol Ziaja <kziaja34@gmail.com>
2025-10-02 14:27:02 +0200

    made FV generation endpoint

---
+++ SSSMCR/SSSMCR.ApiService/Controller/InvoiceController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SSSMCR.ApiService.Services;
+using System.Threading.Tasks;
+
+namespace SSSMCR.ApiService.Controller
+{
+    [Route("api/invoices")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        private readonly IInvoiceService _invoiceService;
+
+        public InvoiceController(IInvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+        
+        [HttpGet("generate/{orderId}")]
+        public async Task<IActionResult> GenerateInvoice(int orderId)
+        {
+            try
+            {
+                var pdfDocument = await _invoiceService.GetInvoice(orderId);
+                
+                //pdfDocument.Save("Invoice.pdf");
+                
+                MemoryStream stream = new MemoryStream();
+                pdfDocument.Save(stream);
+                
+                Response.ContentType = "application/pdf";
+                Response.Headers.Add("content-length", stream.Length.ToString());
+                byte[] bytes = stream.ToArray();
+                stream.Close();
+                
+                return File(bytes, "application/pdf", "Invoice.pdf");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
